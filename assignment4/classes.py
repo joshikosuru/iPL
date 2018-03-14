d1b7efaf268df428837560f853a9137202d17cf0
joshikosuru <joshi010198@gmail.com>
2018-03-14 15:03:00 +0530

    small changes to ass constants

--- assignment4/classes.py
+++ assignment4/classes.py
@@ -1,32 +1,4 @@
-binaryOperators = ["LE", "GE", "LT", "GT", "NE", "EQ", "OR", "AND", "PLUS", "MINUS", "MUL", "DIV", "ASGN"]
-unaryOperators = ["VAR", "ADDR", "UMINUS", "CONST", "DEREF", "NOT"]
-binaryOpMap = {
-	"LE" : "<=",
-	"GE" : ">=",
-	"LT" : "<",
-	"GT" : ">",
-	"NE" : "!=",
-	"EQ" : "==",
-	"OR" : "||",
-	"AND" : "&&",
-	"PLUS" : "+",
-	"MINUS" : "-",
-	"MUL" : "*",
-	"DIV" : "/",
-	"ASGN" : "="
-}
-
-unaryOpMap = {
-"VAR" : "",
-"ADDR" : "&",
-"UMINUS" : "-",
-"CONST" : "",
-"DEREF" : "*",
-"NOT" : "!"
-}
-
-
-
+from constants import *
 
 class Tree(object):
 	def __init__(self):
